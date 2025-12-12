1a9f1ec5ec422dd9a49da2be4fead8c4d0fc3b6a
AbhishekTiwariICIT <homedevendra@gmail.com>
2025-12-12 15:22:36 +0530

    slider completed

--- frontend/src/Components/Carousel.jsx
+++ frontend/src/Components/Carousel.jsx
@@ -2,29 +2,60 @@ import { useState } from 'react';
 import styles from '../css/Carousel.module.css';
 import { useEffect } from 'react';
 import { useRef } from 'react';
+import { FaGreaterThan, FaLessThan } from "react-icons/fa6";
 
 export default function Carousel({ children }) {
 
     const CarouselBoxRef = useRef();
+    const intervalRef=useRef(0);
 
     const [currentIndex, setCurrentIndex] = useState(0);
 
-   useEffect(() => {
-    const interval = setInterval(() => {
-        
+
+    useEffect(() => {
+        const { slides, count } = getSlidesInfo();
+        slides[0].setAttribute("data-active", "true");
+
+       intervalRef.current = setInterval(() => {
+            setCurrentIndex((prev) => {
+                const { slides, count } = getSlidesInfo();
+                const newIndex = prev === count - 1 ? 0 : prev + 1;
+
+                [...slides].forEach((slide, index) => {
+                    slide.setAttribute('data-active', index === newIndex);
+                })
+                return newIndex;
+            });
+        }, 3000);
+
+        return () => clearInterval(intervalRef.current);   // cleanup
+    }, []);
+
+    function startSlider(){
+
+    }
+    function handlePrevious() {
+
+    }
+
+    function handleNext() {
+        clearInterval(intervalRef);
+        const { slides, count } = getSlidesInfo();
+        const newIndex = currentIndex === count - 1 ? 0 : currentIndex + 1;
+        [...slides].forEach((slide, index) => {
+            slide.setAttribute('data-active', index === newIndex);
+        });
+        setCurrentIndex(newIndex)
+    }
+
+    function getSlidesInfo() {
         const CarouselBox = CarouselBoxRef.current;
-            const slides=CarouselBox.children;
-            console.log(slides)
-            const count = slides.length;
-            console.log(count)
-        
-        setCurrentIndex(prev => prev + 1);
-    }, 3000);
-
-    return () => clearInterval(interval);   // cleanup
-}, []);
-
-
+        const slides = CarouselBox.children;
+        console.log(slides)
+        const count = slides.length;
+        console.log(count)
+        return { slides, count };
+    }
 
     return (
         <>
@@ -32,6 +63,18 @@ export default function Carousel({ children }) {
             {currentIndex}
             <div className={styles.imagebox} ref={CarouselBoxRef}>
                 {children}
+            </div>
+            <div className={`${styles.sliderBtn}`}>
+                <div className={`${styles.sliderBtnEffect} ${styles.sliderLeftBtn}`} onClick={handlePrevious}>
+                    <FaLessThan />
+                </div>
+
+                <div className={`${styles.sliderBtnEffect} ${styles.sliderLeftBtn}`} onClick={handleNext}>
+                    <FaGreaterThan />
+                </div>
+
+
+
             </div>
         </>
     )
