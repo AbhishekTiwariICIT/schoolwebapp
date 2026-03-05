f5d0e9dc7ae7de8a129addb6358aaca17448496c
Abhishek Tiwari <homedevendra@gmail.com>
2026-03-04 22:19:08 -0800

    topper student sectio desktop version maked

--- frontend/src/Components/schResultsUpdate/TopperStd.jsx
+++ frontend/src/Components/schResultsUpdate/TopperStd.jsx
@@ -1,6 +1,24 @@
 import styles from '../../css/schResultsUpdate/topperStd.module.css';
-
+import htp from '../../assets/Images/htp.jpg';
+import itp from '../../assets/Images/itp.jpg';
 function TopperStd({ data }) {
+
+    const topperStdDetails = [
+        {
+            name: "Skashi",
+            per: 98,
+            rank: 1,
+            disRank: 1,
+            url: { itp }
+        },
+        {
+            name: `KHUSHI SAHU`,
+            per: 97,
+            rank: 2,
+            disRank: 3,
+            url: { htp }
+        }
+    ];
 
     const linkdata = [
         {
@@ -27,13 +45,36 @@ function TopperStd({ data }) {
                 </div>
                 <div className={styles.SectionSubTitleBox}>
                     {data.map((item) => (
-                        <div className={styles.sectionSubTitle}>
-                            <span>{item.SubTitle}</span>
-                        </div>
+                        <div className={styles.topperStdContent}>
 
+                            <div className={styles.sectionSubTitle}>
+                                <span>{item.SubTitle}</span>
+                            </div>
+                            <div className={styles.stdImgSection}>
+                                <div className={`${styles.imgbox} ${styles.stdbox}`}>
+                                    <img src={htp} alt="image not found" />
+                                </div>
+                                <div className={`${styles.stdDetails} ${styles.stdbox}`}>
+                                    <table className={styles.stdTable}>
+                                        {topperStdDetails.map((items) =>
+                                        (
+                                            <>
+                                                <tr>
+                                                    <td>Name</td>
+                                                    <td>{items.name}</td>
+                                                </tr>
+                                                
+                                            </>
+                                        ))}
+
+                                    </table>
+                                </div>
+                            </div>
+
+
+
+                        </div>
                     ))}
-                </div>
-                <div className={styles.grid}>
                 </div>
             </div>
             <div className={styles.latestNewsSection}>
